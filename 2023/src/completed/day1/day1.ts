1bab15e44532b2083e85e2cd6f516c7ae2690579
Kat <mercier200012@hotmail.com>
2024-12-04 22:44:30 -0500

    Day 1 2023 push

---
+++ 2023/src/completed/day1/day1.ts
@@ -0,0 +1,32 @@
+import { testInput, day1Input } from "./input";
+
+const numberParse = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+const mapNumber = (x: string) => {
+    if (numberParse.includes(x)) {
+        return numberParse.indexOf(x) + 1;
+    }
+    return Number(x);
+}
+
+const getNumber = (input: string) => {
+    const regex = /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g
+    const matches = [...input.matchAll(regex)];
+    const parsed = matches.map(x => mapNumber(x[1]));
+    if (matches.length === 1) {
+        return Number(parsed[0].toString().concat(parsed[0].toString()));
+    } else {
+        return Number(parsed[0].toString().concat(parsed[parsed.length - 1].toString()))
+    }
+}
+
+const run = () => {
+    let count = 0;
+    for (let i = 0; i < day1Input.length; i++) {
+        const number = getNumber(day1Input[i]);
+        count += number;
+    }
+    console.log(`The total is ${count}`);
+}
+
+run();
