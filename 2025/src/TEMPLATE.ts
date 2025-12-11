e75e0503c388ac1787c3e804ccefe1680c67da26
Kat <mercier200012@hotmail.com>
2025-12-11 14:46:23 -0500

    Adding 2025 start

---
+++ 2025/src/TEMPLATE.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from 'fs';
+
+const input1 = readFileSync('./inputs/day01-1.txt', 'utf-8').trim();
+const input2 = readFileSync('./inputs/day01-2.txt', 'utf-8').trim();
+
+function part1(input: string): number {
+    const lines = input.split('\n');
+
+
+
+    return 0;
+}
+
+function part2(input: string): number {
+    const lines = input.split('\n');
+
+
+
+    return 0;
+}
+
+console.log('Part 1:', part1(input1));
+console.log('Part 2:', part2(input2));
