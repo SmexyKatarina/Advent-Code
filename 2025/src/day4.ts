6f61523f8d8421fa27a7cfc42b4b70c2cc87eb56
Kat <mercier200012@hotmail.com>
2025-12-17 00:40:52 -0500

    Optimize 2025 - Day 4

--- 2025/src/day4.ts
+++ 2025/src/day4.ts
@@ -11,8 +11,8 @@ function part1(input: string): number {
     const papers = grid
         .getGrid() // Get Grid
         .flat() // Flatten the array
-        .filter(cell => // Filter the cells
-            cell.cellValue === "@" && // Filter where it equals a paper
+        .filter(cell => cell.cellValue === "@") // Filter the cells for only paper
+        .filter(cell =>
             grid.getAdjacentCells(cell) // And that the adjacent cells to this one
                 .filter(y => y.cellValue === "@") // Where the ones that are paper
                 .length < 4 // Are totaled to less than 4
@@ -24,21 +24,21 @@ function part1(input: string): number {
 function part2(input: string): number {
     console.time("Part 2 Process Time");
     const grid: Grid = new Grid(input);
-
     let totalRemoved = 0;
-
     while (true) {
 
         // Cells of accessable papers
         const accessablePapers = grid
             .getGrid() // Get Grid
             .flat() // Flatten the array
-            .filter(cell => // Filter the cells
-                cell.cellValue === "@" && // Filter where it equals a paper
-                grid.getAdjacentCells(cell) // And that the adjacent cells to this one
+            .filter(cell => {
+                // If not a paper cell, skip it.
+                if (cell.cellValue !== "@") return false;
+                // Then check this paper cell and its adjacent cells
+                return grid.getAdjacentCells(cell) // And that the adjacent cells to this one
                     .filter(y => y.cellValue === "@") // Where the ones that are paper
                     .length < 4 // Are totaled to less than 4
-            );
+            })
         // If there is no more accaccessible paper cells, finish.
         if (accessablePapers.length === 0) break;
         // Loop through and change them to empty cells while counting the removed paper
@@ -47,12 +47,11 @@ function part2(input: string): number {
             totalRemoved++;
         }
     }
-
     console.timeEnd("Part 2 Process Time");
     return totalRemoved;
 }
 
 //console.log("Part 1 (Test Input):", part1(testInput));
 //console.log("Part 2 (Test Input):", part2(testInput));
-//console.log('Part 1:', part1(input));
+console.log('Part 1:', part1(input));
 console.log('Part 2:', part2(input));
